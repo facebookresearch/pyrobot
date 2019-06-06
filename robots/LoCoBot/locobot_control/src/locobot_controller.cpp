c4f5170172651c908c3cf12d206137ca81933dde
kalyanvasudev <kalyan051993@gmail.com>
2019-06-06 14:18:41 -0400

    License

--- robots/LoCoBot/locobot_control/src/locobot_controller.cpp
+++ robots/LoCoBot/locobot_control/src/locobot_controller.cpp
@@ -1,4 +1,8 @@
 /*******************************************************************************
+* Copyright (c) Facebook, Inc. and its affiliates.
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+
 * Copyright 2018 ROBOTIS CO., LTD.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
