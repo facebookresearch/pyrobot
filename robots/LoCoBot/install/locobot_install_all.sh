92d1d9c19148e3156869537c942a8820337d9d1a
kalyanvasudev <kalyan051993@gmail.com>
2019-06-12 10:54:32 -0400

    updated key in the installation script

--- robots/LoCoBot/install/locobot_install_all.sh
+++ robots/LoCoBot/install/locobot_install_all.sh
@@ -52,7 +52,7 @@ sudo pip install --upgrade pip
 if [ $(dpkg-query -W -f='${Status}' ros-kinetic-desktop-full 2>/dev/null | grep -c "ok installed") -eq 0 ]; then 
 	echo "Installing ROS..."
 	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-	sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+	sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 	sudo apt-get update
 	sudo apt-get -y install ros-kinetic-desktop-full
 	if [ -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
