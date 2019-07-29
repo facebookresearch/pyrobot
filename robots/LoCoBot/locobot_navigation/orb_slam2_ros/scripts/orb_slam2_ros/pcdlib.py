e59b0fcf4e6a555c3739c6335448ab46989347b8
kalyanvasudev <kalyan051993@gmail.com>
2019-07-29 14:43:02 -0400

    made orbslam imports work in python3

--- robots/LoCoBot/locobot_navigation/orb_slam2_ros/scripts/orb_slam2_ros/pcdlib.py
+++ robots/LoCoBot/locobot_navigation/orb_slam2_ros/scripts/orb_slam2_ros/pcdlib.py
@@ -10,7 +10,13 @@ import threading
 import time
 from os.path import expanduser
 
-import cv2
+import sys
+ros_path = '/opt/ros/kinetic/lib/python2.7/dist-packages'
+if ros_path in sys.path:
+    sys.path.remove(ros_path)
+    import cv2
+sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
+
 import numpy as np
 import rospy
 import tf.transformations
