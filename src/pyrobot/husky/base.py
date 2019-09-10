c0ac0d12e354ed09183dbefe8f7a3709c633baa8
harry-savioke <harry@savioke.com>
2019-09-10 21:11:37 +0800

    modify husky.py

--- src/pyrobot/husky/base.py
+++ src/pyrobot/husky/base.py
@@ -29,9 +29,9 @@ from std_msgs.msg import Empty
 from pyrobot.core import Base
 from std_msgs.msg import Empty
 
-from base_control_utils import MoveBasePlanner, _get_absolute_pose
-from base_controllers import ProportionalControl, ILQRControl, MoveBaseControl
-from bicycle_model import wrap_theta
+from pyrobot.locobot.base_control_utils import MoveBasePlanner, _get_absolute_pose
+from pyrobot.locobot.base_controllers import ProportionalControl, ILQRControl, MoveBaseControl
+from pyrobot.locobot.bicycle_model import wrap_theta
 
 
 
