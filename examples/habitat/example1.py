101abddd23778c32bd21c1fac60dd9cdcc81e6b0
kalyanvasudev <kalyan051993@gmail.com>
2019-10-11 17:37:15 -0400

    Updated the example

--- examples/habitat/example1.py
+++ examples/habitat/example1.py
@@ -9,26 +9,35 @@ if ros_path in sys.path:
 	import cv2
 sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
 
+
+def visualize(bot):
+	rgb_img = bot.camera.get_rgb()
+	cv2.imshow('Color', rgb_img[..., 0:3][..., ::-1])
+	base_state = bot.base.get_state()
+	print("Base State:")
+	print(base_state)
+	cv2.waitKey(4000)
+
 # Please change this to match your habitat_sim repo's path
-path_to_habitat_sim = "/home/kalyan/temp/habitat-sim"
+path_to_habitat_sim = "/home/kalyanv/temp/habitat-sim"
 relative_path = "examples/data/scene_datasets/habitat-test-scenes/skokloster-castle.glb"
 
 common_config = dict(scene_path=os.path.join(path_to_habitat_sim, relative_path))
 bot = Robot('habitat', common_config=common_config)
-
-rgb_img = bot.camera.get_rgb()
-cv2.imshow('Color', rgb_img[..., 0:3][..., ::-1])
-base_state = bot.base.get_state()
-print("Base State:")
-print(base_state)
-cv2.waitKey(4000)
+visualize(bot)
 
 # Execute an action on the base to move forward
 bot.base.execute_action("move_forward")
+print("Move forward using discreate actions")
+visualize(bot)
 
-rgb_img = bot.camera.get_rgb()
-cv2.imshow('Color', rgb_img[..., 0:3][..., ::-1])
-base_state = bot.base.get_state()
-print("Base State:")
-print(base_state)
-cv2.waitKey(4000)
+# Make the agent go to a relative pose
+print("Move the robot to a relative pose")
+bot.base.go_to_relative([1.0, 1.0, 0.0])
+visualize(bot)
+
+# Make the agent adjust its camera motors
+print("Set pan/tilt camera motor values.")
+bot.camera.set_pan(0.1)
+visualize(bot)
+
