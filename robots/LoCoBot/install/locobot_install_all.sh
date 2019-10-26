a010a58a9e48c36e2236083c209a6230636301c5
kalyanvasudev <kalyan051993@gmail.com>
2019-10-25 22:39:51 -0400

    Fix instructions to always pull master branch

--- robots/LoCoBot/install/locobot_install_all.sh
+++ robots/LoCoBot/install/locobot_install_all.sh
@@ -183,10 +183,6 @@ fi
 if [ ! -d "$LOCOBOT_FOLDER/src/pyrobot" ]; then
 	cd $LOCOBOT_FOLDER/src
 	git clone --recurse-submodules https://github.com/facebookresearch/pyrobot.git
-	if [ $PYTHON_VERSION == "3" ]; then
-		cd pyrobot
-		git checkout python3
-	fi
 fi
 cd $LOCOBOT_FOLDER
 rosdep update 
