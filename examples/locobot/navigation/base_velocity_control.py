186e458492b63f0786aeb69e7cd604482b2ed3dc
kalyan <kalyan051993@gmail.com>
2020-02-05 14:22:37 -0500

    Hydra example commit

--- examples/locobot/navigation/base_velocity_control.py
+++ examples/locobot/navigation/base_velocity_control.py
@@ -3,27 +3,20 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from absl import flags, app
+
 from pyrobot import Robot
+import hydra
 
-FLAGS = flags.FLAGS
-flags.DEFINE_string('base_controller', 'ilqr', 'One of ilqr, proportional, movebase.')
-flags.DEFINE_string('base_planner', 'movebase', 'movebase or none')
-flags.DEFINE_string('botname', 'locobot', 'Robot name, locobot, locobot_lite, ...')
-flags.DEFINE_float('linear_speed', 0.2, 'Linear speed.')
-flags.DEFINE_float('angular_speed', 0.5, 'Angular velocity.')
-flags.DEFINE_float('duration', 1.0, 'Time to run velocity commands for.')
-
-
-def main(_):
-    bot = Robot(FLAGS.botname, use_arm=False,
-                base_config={'base_planner': FLAGS.base_planner,
-                             'base_controller': FLAGS.base_controller})
-    bot.base.set_vel(fwd_speed=FLAGS.linear_speed,
-                     turn_speed=FLAGS.angular_speed,
-                     exe_time=FLAGS.duration)
+@hydra.main(config_path="base_velocity_control.yaml")
+def main(cfg):
+    bot = Robot(cfg.botname, use_arm=False,
+                base_config={'base_planner': cfg.base_planner,
+                             'base_controller': cfg.base_controller})
+    bot.base.set_vel(fwd_speed=cfg.linear_speed,
+                     turn_speed=cfg.angular_speed,
+                     exe_time=cfg.duration)
     bot.base.stop()
 
 
 if __name__ == '__main__':
-    app.run(main)
+    main()
