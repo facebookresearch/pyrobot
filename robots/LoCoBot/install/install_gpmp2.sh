3b70f1fe10176f804350d3a82732acf1a74b89c1
kalyanvasudev <kalyan051993@gmail.com>
2020-04-09 21:42:15 -0400

    add checks to gpmp install

--- robots/LoCoBot/install/install_gpmp2.sh
+++ robots/LoCoBot/install/install_gpmp2.sh
@@ -14,29 +14,33 @@ sudo apt-get -y install python-numpy \
 pip install cython
 
 GPMP_FOLDER=~/gpmp_ws
-
-# Clone GTSAM and GPMP2
-mkdir -p $GPMP_FOLDER
-cd $GPMP_FOLDER
-git clone https://github.com/kalyanvasudev/gpmp2.git
-git clone https://github.com/borglab/gtsam.git
+if [ ! -d "$GPMP_FOLDER" ]; then
+	mkdir -p $GPMP_FOLDER
+fi
 
 # Install GTSAM
-cd $GPMP_FOLDER/gtsam
-git checkout wrap-export
-mkdir build
-cd build
-cmake -DGPMP2_BUILD_PYTHON_TOOLBOX:=ON ..
-sudo make install -j8
+if [ ! -d "$GPMP_FOLDER/gtsam" ]; then
+	cd $GPMP_FOLDER
+	git clone https://github.com/borglab/gtsam.git
+	cd gtsam
+	git checkout wrap-export
+	mkdir build
+	cd build
+	cmake -DGPMP2_BUILD_PYTHON_TOOLBOX:=ON ..
+	sudo make install -j8
+	# Update pythonpath in bashrc to recognize GPMP2
+	echo "export PYTHONPATH=$PYTHONPATH:/usr/local/cython" >> ~/.bashrc
+	source ~/.bashrc
+fi
 
 # Install GPMP2
-cd $GPMP_FOLDER/gpmp2
-mkdir build
-cd build
-cmake -DGPMP2_BUILD_PYTHON_TOOLBOX:=ON ..
-sudo make install -j8
 
-
-# Update pythonpath in bashrc to recognize GPMP2
-echo "export PYTHONPATH=$PYTHONPATH:/usr/local/cython" >> ~/.bashrc
-source ~/.bashrc
+if [ ! -d "$GPMP_FOLDER/gpmp2" ]; then
+	cd $GPMP_FOLDER
+	git clone https://github.com/kalyanvasudev/gpmp2.git
+	cd gpmp2
+	mkdir build
+	cd build
+	cmake -DGPMP2_BUILD_PYTHON_TOOLBOX:=ON ..
+	sudo make install -j8
+fi
