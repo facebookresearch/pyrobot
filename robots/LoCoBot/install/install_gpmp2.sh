be3b5f22792ea04a3a9c69fcc6b004954b3b0960
locobot <kalyan051993@gmail.com>
2020-05-02 23:16:08 -0400

    remove ipython form gpmp2 install

--- robots/LoCoBot/install/install_gpmp2.sh
+++ robots/LoCoBot/install/install_gpmp2.sh
@@ -4,8 +4,6 @@
 sudo apt-get -y install python-numpy \
 						python-scipy \
 						python-matplotlib \
-						ipython \
-						ipython-notebook \
 						python-pandas \
 						python-sympy \
 						python-nose \
