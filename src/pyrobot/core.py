6371c5265872f822b22176d3d58cc1aaa497a5b6
locobot <kalyan051993@gmail.com>
2020-05-13 20:08:33 -0400

    make depth measurements to always use meters

--- src/pyrobot/core.py
+++ src/pyrobot/core.py
@@ -385,6 +385,7 @@ class Camera(object):
         self.camera_img_lock.acquire()
         depth = copy.deepcopy(self.depth_img)
         self.camera_img_lock.release()
+        depth = depth / self.configs.CAMERA.DEPTH_MAP_FACTOR
         return depth
 
     def get_rgb_depth(self):
