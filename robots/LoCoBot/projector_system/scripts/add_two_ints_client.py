0a491aa6e937d657b2f78e6f1c5053cffb3723dd
locobot_user <locobot@locobot.com>
2020-07-01 09:58:26 -0600

    working with services for projector_system

---
+++ robots/LoCoBot/projector_system/scripts/add_two_ints_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+import rospy
+from projector_system.srv import *
+
+def add_two_ints_client(x, y):
+    rospy.wait_for_service('add_two_ints')
+    try:
+        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
+        resp1 = add_two_ints(x, y)
+        return resp1.sum
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+
+def usage():
+    return "%s [x y]"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+    else:
+        print(usage())
+        sys.exit(1)
+    print("Requesting %s+%s"%(x, y))
+    print("%s + %s = %s"%(x, y, add_two_ints_client(x, y)))
