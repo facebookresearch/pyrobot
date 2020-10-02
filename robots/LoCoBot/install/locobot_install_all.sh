4b4dea0ebc8ecf807b77d07ba14b5355e8a0df82
Zisu Dong <jerry_1021@msn.cn>
2020-10-02 18:03:13 -0400

    test updated

--- robots/LoCoBot/install/locobot_install_all.sh
+++ robots/LoCoBot/install/locobot_install_all.sh
@@ -411,10 +411,10 @@ if [ $INSTALL_TYPE == "full" ]; then
 	sudo usermod -a -G dialout $USER
 fi
 
-# Step 8 - Install GPMP specific things
-cd $LOCOBOT_FOLDER/src/pyrobot/robots/LoCoBot/install
-chmod +x install_gpmp2.sh
-source install_gpmp2.sh
+# # Step 8 - Install GPMP specific things
+# cd $LOCOBOT_FOLDER/src/pyrobot/robots/LoCoBot/install
+# chmod +x install_gpmp2.sh
+# source install_gpmp2.sh
 
 end_time="$(date -u +%s)"
 elapsed="$(($end_time-$start_time))"
