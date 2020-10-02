5e50337f50533e1c4f67c0f02e2dd275b87ea466
Zisu Dong <jerry_1021@msn.cn>
2020-10-02 16:26:38 -0400

    test updated

--- tests/run_test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# show result in web browser
-python run_test.py --out_dir htmlcov/`git rev-parse HEAD`
-## not show result in web browser
-#python run_test.py --out_dir htmlcov/`git rev-parse HEAD` --nobrowser
