6740950e9498e8eea05c7dbbe27164fea178d4ab
Zisu Dong <jerry_1021@msn.cn>
2020-10-08 11:37:20 -0400

    light weight setup

--- robots/LoCoBot/install/locobot_install_all.sh
+++ robots/LoCoBot/install/locobot_install_all.sh
@@ -238,7 +238,7 @@ if [ ! -d "$LOCOBOT_FOLDER/src/pyrobot" ]; then
 	cd $LOCOBOT_FOLDER/src
 	git clone https://github.com/facebookresearch/pyrobot.git
 	cd pyrobot
-	git checkout Develop
+	git checkout API_0.4
 	git submodule update --init --recursive
   if [ $LOCOBOT_PLATFORM == "cmu" ]; then
     cd $LOCOBOT_FOLDER/src/pyrobot/robots/LoCoBot/locobot_description/urdf
