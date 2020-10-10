ec2693a7d94767b139cc34d303e5e28b656c1be9
Zisu Dong <jerry_1021@msn.cn>
2020-10-09 21:27:01 -0400

    circleci test

--- robots/LoCoBot/install/locobot_install_all.sh
+++ robots/LoCoBot/install/locobot_install_all.sh
@@ -135,7 +135,7 @@ if [ $ROS_NAME == "kinetic" ]; then
 else
 	if [ $(dpkg-query -W -f='${Status}' ros-melodic-desktop-full 2>/dev/null | grep -c "ok installed") -eq 0 ]; then 
 		echo "Installing ROS..."
-		sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu bionic main" > /etc/apt/sources.list.d/ros1-latest.list'
+		sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu bionic main" > /etc/apt/sources.list.d/ros-latest.list'
 		sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 		sudo apt-get update
 		sudo apt-get -y install ros-melodic-desktop-full
