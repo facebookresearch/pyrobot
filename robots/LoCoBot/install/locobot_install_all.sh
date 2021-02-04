96c43277bfd10afffbb22b1e7ec60a65ebdb3bdc
Zisu Dong <j_dong1021@berkeley.edu>
2021-02-04 13:54:27 -0500

    fix vslam

--- robots/LoCoBot/install/locobot_install_all.sh
+++ robots/LoCoBot/install/locobot_install_all.sh
@@ -268,7 +268,7 @@ if [ ! -d "$LOCOBOT_FOLDER/src/pyrobot/robots/LoCoBot/thirdparty" ]; then
 		git clone https://github.com/ROBOTIS-GIT/DynamixelSDK.git
 		git clone https://github.com/ROBOTIS-GIT/dynamixel-workbench-msgs.git
 		git clone https://github.com/ros-controls/ros_control.git
-		git clone https://github.com/kalyanvasudev/ORB_SLAM2.git
+		git clone https://github.com/taochenshh/ORB_SLAM2.git
 		git clone https://github.com/s-gupta/ar_track_alvar.git
 
 	if [ $ROS_NAME == "kinetic" ]; then
@@ -277,7 +277,6 @@ if [ ! -d "$LOCOBOT_FOLDER/src/pyrobot/robots/LoCoBot/thirdparty" ]; then
 		cd DynamixelSDK && git checkout 05dcc5c551598b4d323bf1fb4b9d1ee03ad1dfd9 && cd ..
 		cd dynamixel-workbench-msgs && git checkout 93856f5d3926e4d7a63055c04a3671872799cc86 && cd ..
 		cd ros_control && git checkout 44cf68aab6cb1293e91f69ef7efe30b80195356b && cd ..
-		cd ORB_SLAM2 && git checkout ec8d750d3fc813fe5cef82f16d5cc11ddfc7bb3d && cd ..
 		cd ar_track_alvar && git checkout 625a3cf928552ee5bf97453897af6790d523962f && cd ..
 	else
 		cd create_autonomy && git checkout 90e597ea4d85cde1ec32a1d43ea2dd0b4cbf481c && cd ..
@@ -285,9 +284,11 @@ if [ ! -d "$LOCOBOT_FOLDER/src/pyrobot/robots/LoCoBot/thirdparty" ]; then
 		cd DynamixelSDK && git checkout 05dcc5c551598b4d323bf1fb4b9d1ee03ad1dfd9 && cd ..
 		cd dynamixel-workbench-msgs && git checkout 93856f5d3926e4d7a63055c04a3671872799cc86 && cd ..
 		cd ros_control && git checkout cd39acfdb2d08dc218d04ff98856b0e6a525e702 && cd ..
-		cd ORB_SLAM2 && git checkout ec8d750d3fc813fe5cef82f16d5cc11ddfc7bb3d && cd ..
 		cd ar_track_alvar && git checkout a870d5f00a548acb346bfcc89d42b997771d71a3 && cd ..
 	fi
+	cd ORB_SLAM2
+	chmod +x install.sh
+	sudo bash -i install.sh
 fi
 
 cd $LOCOBOT_FOLDER
